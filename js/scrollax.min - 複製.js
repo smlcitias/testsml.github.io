5f7f24fb82062c5d30329d067d63867191eaefc4
smlcitias <42274021+smlcitias@users.noreply.github.com>
2020-10-25 20:13:57 +0800

    update

---
+++ js/scrollax.min - 複製.js
@@ -0,0 +1,14 @@
+/**
+ 手機menu
+ * 
+ */
+$(function(){
+    var sideslider = $(‘[data-toggle=collapse-side]’);
+    var get_sidebar = sideslider.attr(‘data-target-sidebar’);
+    var get_content = sideslider.attr(‘data-target-content’);
+    sideslider.click(function(event){
+    $(get_sidebar).toggleClass(‘in’);
+    $(get_content).toggleClass(‘out’);
+    });
+    });
+    [/javascript]
