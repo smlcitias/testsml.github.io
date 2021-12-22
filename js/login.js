e065bc165188a1d6dc7dd5f08713b4f4fe582286
smlcitias <42274021+smlcitias@users.noreply.github.com>
2021-12-22 14:34:06 +0800

    update

--- js/login.js
+++
@@ -1,9 +0,0 @@
-function to open(){
-  if(document.getElementById("password").value=="Bioasp"){
-    document.location="Resources.html";
-       return true;
-    }else{
-    alert("密碼錯誤，請再輸入一次！")
-    return false;
-    }
-  });
--- js/return_to_top.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-  if ($(this).scrollTop() >= 1800) {
-      $('#return-to-top').fadeIn(500);
-  } else {
-      $('#return-to-top').fadeOut(500);
-  }
-});
